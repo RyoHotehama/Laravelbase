44d7954b4454051393562534956071278e9fb70d
ryo hotehama <>
2022-07-01 12:06:52 +0900

    バリデーションの変更

--- src/swimpractice/app/Http/Controllers/SwimController.php
+++ src/swimpractice/app/Http/Controllers/SwimController.php
@@ -3,19 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\HTTP\Requests\SwimRequest;
+use Validator;
 
 class SwimController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('Swim.index');
     }
 
-    public function practice() {
+    public function practice()
+    {
         return view('Swim.create_practice');
     }
 
-    public function post(SwimRequest $request) {
+    public function post(Request $request)
+    {
+        $rules = [
+            'date' => 'required|after_or_equal:today',
+            'distance' => 'required|integer',
+            'number' => 'required|integer',
+            'set' => 'required|integer',
+            'time' => 'required',
+        ];
+        $messages = [
+            'date.required' => '日付は必ず入力してください。',
+            'date.after_or_equal' => '本日以降の日付で入力してください。',
+            'distance.required' => '距離が入力されていません。',
+            'distance.integer' => '距離の値が不正です。',
+            'number.required' => '本数が入力されていません。',
+            'number.integer' => '本数の値が不正です。',
+            'set.required' => 'セット数が入力されていません。',
+            'set.integer' => 'セット数が不正です。',
+            'time.required' => 'サイクルが入力されていません。',
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()) {
+            return redirect('/swim/practice')->withErrors($validator)->withInput();
+        }
         return view ('Swim.create_practice');
     }
 }
